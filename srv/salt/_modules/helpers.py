c97e18a1f4d5b1250d741e24a561ab9b7072cb95
Piotr Mazurkiewicz <dzolnierz@gmail.com>
2021-12-13 19:52:57 +0100

    Add deep_sort

--- srv/salt/_modules/helpers.py
+++ srv/salt/_modules/helpers.py
@@ -1,4 +1,5 @@
 import re
+from collections import OrderedDict
 
 
 def splitpart(value, index, char=','):
@@ -9,6 +10,20 @@ def splitpart(value, index, char=','):
 
 def parse_debver(value):
     try:
-        return re.findall(r"(?:\d+:)?(.*)~", value)[0]
+        return re.findall(r"(?:\d+:)?([^~]+)", value)[0]
     except:
         return ''
+
+
+def deep_sort(obj):
+    if isinstance(obj, dict):
+        obj = OrderedDict(sorted(obj.items()))
+        for k, v in obj.items():
+            if isinstance(v, dict) or isinstance(v, list):
+                obj[k] = deep_sort(v)
+    if isinstance(obj, list):
+        for i, v in enumerate(obj):
+            if isinstance(v, dict) or isinstance(v, list):
+                obj[i] = deep_sort(v)
+        obj = sorted(obj, key=lambda x: json.dumps(x))
+    return obj
